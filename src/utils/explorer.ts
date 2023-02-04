5c54b28fe014a013c613d55baa1d602462c0ebaf
SUBHAM KUNWAR <68282784+rabbit-sol@users.noreply.github.com>
2023-02-04 19:12:10 +0530

    Update explorer.ts

--- src/utils/explorer.ts
+++ src/utils/explorer.ts
@@ -12,8 +12,8 @@ export function getExplorerUrl(
         cluster = `custom&customUrl=${encodeURIComponent(
           'http://127.0.0.1:8899'
         )}`
-      } else if (endpoint === 'https://api.devnet.solana.com') {
-        cluster = 'devnet'
+      } else if (endpoint === 'https://solana-mainnet.g.alchemy.com/v2/roMKSSyXWVrSgFgBLhXoRW_-Y1zIciII') {
+        cluster = 'mainnet-beta'
       }
   
       return cluster ? `?cluster=${cluster}` : ''
